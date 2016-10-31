bebd79a77c38e98445b1a560372b74cd598f9c6b
Chau Tsun Man <tsunman.chau96@gmail.com>
2016-10-31 11:28:40 +0800

    Implement the login and logout function

--- app/js/login.js
+++ app/js/login.js
@@ -1,4 +1,36 @@
 angular.module('teamform-login-app', ['firebase', 'ngMaterial'])
 .controller('LoginCtrl', function($scope, $firebaseObject, $firebaseArray) {
     initializeFirebase();
+
+    var provider = new firebase.auth.FacebookAuthProvider();
+
+    // login function
+    $scope.login = function() {
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            console.log(user);
+        }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+        });
+    };
+
+    // logout function
+    $scope.logout = function() {
+        firebase.auth().signOut().then(function() {
+            // Sign-out successful.
+            console.log('sign-out successful');
+        }, function(error) {
+            // An error happened.
+            console.log('sign-out error');
+        });
+    };
 });
