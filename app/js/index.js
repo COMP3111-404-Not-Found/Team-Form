4bb9ebf412ca9f525083c783f6ccf22341652b61
Chau Tsun Man <tsunman.chau96@gmail.com>
2016-10-31 11:59:25 +0800

    Implement Facebook authentication in the index page

--- app/js/index.js
+++ app/js/index.js
@@ -34,12 +34,34 @@ angular.module('teamform-index-app', ['firebase', 'ngMaterial'])
 .controller('IndexCtrl', function($scope, $firebaseObject, $firebaseArray, $window) {
     initializeFirebase();
 
+
+    /* Facebook Authentication */
+    var provider = new firebase.auth.FacebookAuthProvider();
+
+    // login function
+    $scope.login = function() {
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            console.log(user);
+        }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+        });
+    };
+
     // logout function
     $scope.logout = function() {
         firebase.auth().signOut().then(function() {
             // Sign-out successful.
             console.log('sign-out successful');
-            $window.open('/login.html', '_self');
         }, function(error) {
             // An error happened.
             console.log('sign-out error');
