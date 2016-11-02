f26994bdc7cb29ab0d10b0bee2cc41869edb9927
Chau Tsun Man <tsunman.chau96@gmail.com>
2016-11-02 12:27:48 +0800

    Link with the event list

--- app/js/eventteam.js
+++ app/js/eventteam.js
@@ -1,4 +1,6 @@
 angular.module('teamform-eventteam-app', ['firebase', 'ngMaterial'])
 .controller('EventTeamCtrl', function($scope, $firebaseObject, $firebaseArray, $window) {
     initializeFirebase();
+
+    $scope.eventName = getURLParameter("event");
 });
