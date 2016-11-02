d09de7a8442ffe5c9c6d2cd2c6af0047c131bd3d
Chau Tsun Man <tsunman.chau96@gmail.com>
2016-11-02 12:49:20 +0800

    Create a team list

--- app/js/eventteam.js
+++ app/js/eventteam.js
@@ -3,4 +3,11 @@ angular.module('teamform-eventteam-app', ['firebase', 'ngMaterial'])
     initializeFirebase();
 
     $scope.eventName = getURLParameter("event");
+
+
+    /* teams */
+    var teamRef = firebase.database().ref().child($scope.eventName).child("team");
+
+    var teamObj = $firebaseObject(teamRef);
+    teamObj.$bindTo($scope, "teams");
 });
