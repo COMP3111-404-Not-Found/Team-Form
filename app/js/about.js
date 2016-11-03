7a9ff09e069439f69f161e2f151090ff6afd724b
stevenTCL <steven351906@yahoo.com.hk>
2016-11-03 21:53:31 +0800

    indent fixed
    1. indentation fixed
    2. put <ng-app> to <body>

--- app/js/about.js
+++ app/js/about.js
@@ -6,7 +6,7 @@ angular.module('teamform-about-app', ['firebase', 'ngMaterial'])
 .controller('AboutCtrl', function($scope, $firebaseObject, $firebaseArray) {
     initializeFirebase();
     $scope.team = [
-    	{pic: "steven.jpg", name: "Steven To", email: "clto@ust.hk", information: "Hello!"},
-    	{pic: "man.jpg", name: "Man God", email: "ManGod@ust.hk", information: "Hello!!!!!"}
+        {pic: "steven.jpg", name: "Steven To", email: "clto@ust.hk", information: "Hello!"},
+        {pic: "man.jpg", name: "Man God", email: "ManGod@ust.hk", information: "Hello!!!!!"}
     ];
 });
