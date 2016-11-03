62bca17497f733c5b23d13740f0b4963a5bb8f94
AndrewPang <thpangab@connect.ust.hk>
2016-11-03 21:04:42 +0800

    Fixed indentation
    fixed indentation

--- app/js/team.js
+++ app/js/team.js
@@ -11,35 +11,14 @@ $(document).ready(function(){
 
 angular.module('teamform-team-app', ['firebase', 'ngMaterial'])
 .controller('TeamCtrl', ['$scope', '$firebaseObject', '$firebaseArray',
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 	function($scope, $firebaseObject, $firebaseArray) {
+
 	// Call Firebase initialization code defined in site.js
 	initializeFirebase();
 
 	var refPath = "";
 	var eventName = getURLParameter("q");
+
 	// TODO: implementation of MemberCtrl
 	$scope.param = {
 		"teamName" : '',
@@ -50,6 +29,7 @@ angular.module('teamform-team-app', ['firebase', 'ngMaterial'])
 
 	refPath =  eventName + "/admin";
 	retrieveOnceFirebase(firebase, refPath, function(data) {
+
 		if ( data.child("param").val() != null ) {
 			$scope.range = data.child("param").val();
 			$scope.param.currentTeamSize = parseInt(($scope.range.minTeamSize + $scope.range.maxTeamSize)/2);
@@ -61,12 +41,14 @@ angular.module('teamform-team-app', ['firebase', 'ngMaterial'])
 	refPath = eventName + "/member";
 	$scope.member = [];
 	$scope.member = $firebaseArray(firebase.database().ref(refPath));
+
 	refPath = eventName + "/team";
 	$scope.team = [];
 	$scope.team = $firebaseArray(firebase.database().ref(refPath));
 
 	$scope.requests = [];
 	$scope.refreshViewRequestsReceived = function() {
+
 		//$scope.test = "";
 		$scope.requests = [];
 		var teamID = $.trim( $scope.param.teamName );
@@ -81,23 +63,32 @@ angular.module('teamform-team-app', ['firebase', 'ngMaterial'])
 				$scope.requests.push(userID);
 			}
 		});
+
 		$scope.$apply();
+
 	}
+
 	$scope.changeCurrentTeamSize = function(delta) {
 		var newVal = $scope.param.currentTeamSize + delta;
 		if (newVal >= $scope.range.minTeamSize && newVal <= $scope.range.maxTeamSize ) {
 			$scope.param.currentTeamSize = newVal;
 		}
 	}
+
 	$scope.saveFunc = function() {
+
 		var teamID = $.trim( $scope.param.teamName );
+
 		if ( teamID !== '' ) {
+
 			var newData = {
 				'size': $scope.param.currentTeamSize,
 				'teamMembers': $scope.param.teamMembers,
 			};
+
 			var refPath = getURLParameter("q") + "/team/" + teamID;
 			var ref = firebase.database().ref(refPath);
+
 			// for each team members, clear the selection in /[eventName]/team/
 			$.each($scope.param.teamMembers, function(i,obj){
 				//$scope.test += obj;
@@ -105,19 +96,67 @@ angular.module('teamform-team-app', ['firebase', 'ngMaterial'])
 				rec.selection = [];
 				$scope.member.$save(rec);
 			});
+
 			ref.set(newData, function(){
 				// console.log("Success..");
+
 				// Finally, go back to the front-end
 				// window.location.href= "index.html";
 			});
 		}
 	};
+
 	$scope.addSkill = function() {
+		if ($scope.param.team == '') {
+			return;
+		}
 
+		var skillsArray = $firebaseArray(firebase.database().ref(eventName + "/team/" + $scope.param.teamName + "/skills"));
+		skillsArray.$add($scope.param.skill);
+		$scope.param.skill = null;
+	};
 
+	$scope.loadFunc = function() {
 
+		var teamID = $.trim( $scope.param.teamName );
+		var eventName = getURLParameter("q");
+		var refPath = eventName + "/team/" + teamID ;
+		retrieveOnceFirebase(firebase, refPath, function(data) {
 
+			if ( data.child("size").val() != null ) {
+				$scope.param.currentTeamSize = data.child("size").val();
+				$scope.refreshViewRequestsReceived();
+			}
 
+			if ( data.child("teamMembers").val() != null ) {
+				$scope.param.teamMembers = data.child("teamMembers").val();
+			}
 
+			$scope.$apply(); // force to refresh
 
+		});
+	}
+
+	$scope.processRequest = function(r) {
+		//$scope.test = "processRequest: " + r;
+		if (
+			$scope.param.teamMembers.indexOf(r) < 0 &&
+			$scope.param.teamMembers.length < $scope.param.currentTeamSize  ) {
+
+			// Not exists, and the current number of team member is less than the preferred team size
+			$scope.param.teamMembers.push(r);
+
+			$scope.saveFunc();
+		}
+	}
+
+	$scope.removeMember = function(member) {
+
+		var index = $scope.param.teamMembers.indexOf(member);
+		if ( index > -1 ) {
+			$scope.param.teamMembers.splice(index, 1); // remove that item
+
+			$scope.saveFunc();
+		}
+	}
 }]);
