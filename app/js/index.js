f7b580d30680a21e8276db8983d60f0e2f3497ab
AndrewPang <thpangab@connect.ust.hk>
2016-11-15 18:25:49 +0800

    Fixed conflicts
    Fixed conflicts

--- app/js/index.js
+++ app/js/index.js
@@ -40,13 +40,11 @@ angular.module("teamform-index-app", ["firebase", "ngMaterial"])
     };
 
     $scope.user = null;
-    $scope.signedIn = null;
 
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
             // User is signed in.
             console.log(user);
-            $scope.signedIn = true;
 
             // update the user's profile
             var userRef = firebase.database().ref().child("users").child(user.uid);
@@ -61,7 +59,6 @@ angular.module("teamform-index-app", ["firebase", "ngMaterial"])
         } else {
             // No user is signed in.
             console.log('no user is signed in');
-            $scope.signedIn = false;
 
             // refresh the scope
             $scope.$apply(function() {
