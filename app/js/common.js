8f3621bb08442790c4a84a699febd7a96c3c92b3
poojaramalingam <poojaramalingam@gmail.com>
2016-11-15 23:00:51 +0800

    insufficientMemberTeams function completed

--- app/js/common.js
+++ app/js/common.js
@@ -1,1 +1,29 @@
 
+function isAvailable(team) {
+    return team.currentTeamSize < team.size;
+}
+
+
+function getAvailableTeam(teams) {
+    return teams.filter(isAvailable);
+}
+
+function insufficientMemberTeams(teams)
+{
+    var insufficentTeams = getAvailableTeam(teams);
+
+    //if(insufficentTeams.length===0)
+        //return;
+
+    var uids ;
+    for( i=0; i<insufficentTeams.length; i++)
+    {
+        for(var j=0; j<insufficentTeams[i].teamMembers.length; j++)
+        {
+            uids.push(insufficentTeams[i].teamMembers[j].uid);
+
+        }
+    }
+    return uids;
+
+}
