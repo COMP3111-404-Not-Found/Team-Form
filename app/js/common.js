ca4166cdc8fdd7cab6f3a20308cf523a0438dd69
chautsunman <tsunman.chau96@gmail.com>
2016-11-15 17:12:24 +0800

    Revert "Members with no team filtered"

--- app/js/common.js
+++ app/js/common.js
@@ -11,16 +11,3 @@ function isAvailable(team) {
 function getAvailableTeam(teams) {
 	return teams.filter(isAvailable);
 }
-
-/**
- * filter members that do not have a team yet
- *
- * @param members array of members
- * @return filtered members
- */
-function hasNoTeam(member){
-	return member.team===undefined;
-}
-function membersWithNoTeam(members){
-	return members.filter(hasNoTeam);
-}
