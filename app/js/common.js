26e781f05861f438ef3143111b12115d1a28980b
chautsunman <tsunman.chau96@gmail.com>
2016-11-16 10:49:34 +0800

    Test index controller

--- app/js/common.js
+++ app/js/common.js
@@ -80,7 +80,7 @@ function insufficientMemberTeams(teams)
     //if(insufficentTeams.length===0)
         //return;
 
-    var uids ;
+    var uids = [];
     for( i=0; i<insufficentTeams.length; i++)
     {
         for(var j=0; j<insufficentTeams[i].teamMembers.length; j++)
