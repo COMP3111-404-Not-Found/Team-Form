e6cb990f4ff47b661fb3230382d02bf68fdd97a5
Chau Tsun Man <tsunman.chau96@gmail.com>
2016-11-22 16:10:51 +0800

    Sort the recommendations by the number places left

--- app/unit_tests/user_test.js
+++ app/unit_tests/user_test.js
@@ -148,6 +148,160 @@ describe("User Controller", function() {
     });
 
 
+    describe("$scope.sortRecommendationsPlacesLeft", function() {
+        var $scope, controller;
+
+        beforeEach(function() {
+            $scope = {};
+            controller = $controller("UserCtrl", {$scope: $scope, $firebaseObject: $firebaseObject, $firebaseArray: $firebaseArray});
+        });
+
+        it("sort the recommendations by the number of places left descendingly", function() {
+            var recommendations = [
+                {
+                    eventName: "event1",
+                    teams: [
+                        {
+                            teamName: "team1",
+                            placesLeft: 5,
+                            skillsMatch: {match: [], number: 0},
+                            missingSkillsMatch: {match: [], number: 0},
+                            skills: ["Programming"],
+                            teamSkills: ["Programming"]
+                        },
+                        {
+                            teamName: "team2",
+                            placesLeft: 6,
+                            skillsMatch: {match: [], number: 0},
+                            missingSkillsMatch: {match: [], number: 0},
+                            skills: ["Programming"],
+                            teamSkills: ["Programming"]
+                        },
+                        {
+                            teamName: "team3",
+                            placesLeft: 4,
+                            skillsMatch: {match: [], number: 0},
+                            missingSkillsMatch: {match: [], number: 0},
+                            skills: ["Programming"],
+                            teamSkills: ["Programming"]
+                        },
+                        {
+                            teamName: "team4",
+                            placesLeft: 5,
+                            skillsMatch: {match: [], number: 0},
+                            missingSkillsMatch: {match: [], number: 0},
+                            skills: ["Programming"],
+                            teamSkills: ["Programming"]
+                        }
+                    ]
+                },
+                {
+                    eventName: "event2",
+                    teams: [
+                        {
+                            teamName: "team1",
+                            placesLeft: 5,
+                            skillsMatch: {match: [], number: 0},
+                            missingSkillsMatch: {match: [], number: 0},
+                            skills: ["Programming"],
+                            teamSkills: ["Programming"]
+                        },
+                        {
+                            teamName: "team2",
+                            placesLeft: 4,
+                            skillsMatch: {match: [], number: 0},
+                            missingSkillsMatch: {match: [], number: 0},
+                            skills: ["Programming"],
+                            teamSkills: ["Programming"]
+                        },
+                        {
+                            teamName: "team3",
+                            placesLeft: 6,
+                            skillsMatch: {match: [], number: 0},
+                            missingSkillsMatch: {match: [], number: 0},
+                            skills: ["Programming"],
+                            teamSkills: ["Programming"]
+                        }
+                    ]
+                }
+            ];
+
+            var expected = [
+                {
+                    eventName: "event1",
+                    teams: [
+                        {
+                            teamName: "team2",
+                            placesLeft: 6,
+                            skillsMatch: {match: [], number: 0},
+                            missingSkillsMatch: {match: [], number: 0},
+                            skills: ["Programming"],
+                            teamSkills: ["Programming"]
+                        },
+                        {
+                            teamName: "team1",
+                            placesLeft: 5,
+                            skillsMatch: {match: [], number: 0},
+                            missingSkillsMatch: {match: [], number: 0},
+                            skills: ["Programming"],
+                            teamSkills: ["Programming"]
+                        },
+                        {
+                            teamName: "team4",
+                            placesLeft: 5,
+                            skillsMatch: {match: [], number: 0},
+                            missingSkillsMatch: {match: [], number: 0},
+                            skills: ["Programming"],
+                            teamSkills: ["Programming"]
+                        },
+                        {
+                            teamName: "team3",
+                            placesLeft: 4,
+                            skillsMatch: {match: [], number: 0},
+                            missingSkillsMatch: {match: [], number: 0},
+                            skills: ["Programming"],
+                            teamSkills: ["Programming"]
+                        }
+                    ]
+                },
+                {
+                    eventName: "event2",
+                    teams: [
+                        {
+                            teamName: "team3",
+                            placesLeft: 6,
+                            skillsMatch: {match: [], number: 0},
+                            missingSkillsMatch: {match: [], number: 0},
+                            skills: ["Programming"],
+                            teamSkills: ["Programming"]
+                        },
+                        {
+                            teamName: "team1",
+                            placesLeft: 5,
+                            skillsMatch: {match: [], number: 0},
+                            missingSkillsMatch: {match: [], number: 0},
+                            skills: ["Programming"],
+                            teamSkills: ["Programming"]
+                        },
+                        {
+                            teamName: "team2",
+                            placesLeft: 4,
+                            skillsMatch: {match: [], number: 0},
+                            missingSkillsMatch: {match: [], number: 0},
+                            skills: ["Programming"],
+                            teamSkills: ["Programming"]
+                        }
+                    ]
+                }
+            ];
+
+            $scope.sortRecommendationsPlacesLeft(recommendations);
+
+            expect(recommendations).toEqual(expected);
+        });
+    });
+
+
     describe("$scope.limitRecommendations", function() {
         var $scope, controller;
 
@@ -338,7 +492,95 @@ describe("User Controller", function() {
                 }
             ];
 
-            expect($scope.limitRecommendations(recommendations, 5)).toEqual(expected);
+            $scope.limitRecommendations(recommendations, 5);
+
+            expect(recommendations).toEqual(expected);
+        });
+    });
+
+
+    describe("$scope.recommend", function() {
+        var $scope, controller;
+
+        beforeEach(function() {
+            $scope = {};
+            controller = $controller("UserCtrl", {$scope: $scope, $firebaseObject: $firebaseObject, $firebaseArray: $firebaseArray});
+        });
+
+        it("recommend", function() {
+            $scope.eventTeamObj = {
+                event1: {
+                    team: {
+                        team1: {
+                            size: 5,
+                            currentTeamSize: 1,
+                            skills: ["Prgramming"],
+                            teamSkills: ["Programming"]
+                        },
+                        team2: {
+                            size: 6,
+                            currentTeamSize: 1,
+                            skills: ["Prgramming"],
+                            teamSkills: ["Programming"]
+                        },
+                        team3: {
+                            size: 4,
+                            currentTeamSize: 1,
+                            skills: ["Prgramming"],
+                            teamSkills: ["Programming"]
+                        },
+                        team4: {
+                            size: 5,
+                            currentTeamSize: 0,
+                            skills: ["Prgramming"],
+                            teamSkills: ["Programming"]
+                        },
+                        team5: {
+                            size: 5,
+                            currentTeamSize: 0,
+                            skills: ["Prgramming"],
+                            teamSkills: ["Programming"]
+                        },
+                        team6: {
+                            size: 5,
+                            currentTeamSize: 0,
+                            skills: ["Prgramming"],
+                            teamSkills: ["Programming"]
+                        }
+                    }
+                },
+                event2: {
+                    team: {
+                        team1: {
+                            size: 5,
+                            currentTeamSize: 0
+                        }
+                    }
+                },
+                $id: "events"
+            };
+
+            $scope.userObj = {
+                name: "user",
+                skills: ["Programming"],
+                events: {
+                    event1: {
+                        team: "",
+                        selection: ["team1"]
+                    }
+                }
+            };
+
+            $scope.recommend();
+
+            expect($scope.recommendations.length).toEqual(1);
+            expect($scope.recommendations[0].eventName).toEqual("event1");
+            expect($scope.recommendations[0].teams.length).toEqual(5);
+            expect($scope.recommendations[0].teams[0].teamName).toEqual("team2");
+            expect($scope.recommendations[0].teams[1].teamName).toEqual("team4");
+            expect($scope.recommendations[0].teams[2].teamName).toEqual("team5");
+            expect($scope.recommendations[0].teams[3].teamName).toEqual("team6");
+            expect($scope.recommendations[0].teams[4].teamName).toEqual("team1");
         });
     });
 });
