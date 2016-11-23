37cf5b518c292e4aed663e9a4bf864c8cfc99788
chautsunman <tsunman.chau96@gmail.com>
2016-11-23 16:41:52 +0800

    Display development team data (#142)
    #114 
    
    - name
    - email
    - student ID
    - biography
    - profile image

--- app/js/about.js
+++ app/js/about.js
@@ -1,10 +1,9 @@
 angular.module('teamform-about-app', ['firebase', 'ngMaterial'])
 .controller('AboutCtrl', function($scope, $firebaseObject, $firebaseArray) {
     initializeFirebase();
-    $scope.team = [
-        {pic: "steven.jpg", name: "Steven To", email: "clto@ust.hk", information: "Hello!"},
-        {pic: "man.jpg", name: "Man God", email: "ManGod@ust.hk", information: "Hello!!!!!"}
-    ];
+
+    var aboutRef = firebase.database().ref().child("about");
+    $scope.team = $firebaseObject(aboutRef);
 })
 .config(function($mdThemingProvider) {
     $mdThemingProvider.theme('default')
